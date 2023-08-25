f4979755bd8bce097912857d19f9ec627936fc7c
Joshua <23joshtc@gmail.com>
2023-08-24 21:01:21 -0400

    beforeinstallprompt event handler added

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,7 +2,23 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    const deferredPrompt = event;
+
+    butInstall.style.display = 'block'
+    bitInstall.addEvetListener('click', ()=> {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+              } else {
+                console.log('User dismissed the A2HS prompt');
+              }
+              deferredPrompt = null;
+            });
+          });
+        });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
