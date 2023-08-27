d56130de3a05efdb601e739818501aea81ccbc15
Joshua <23joshtc@gmail.com>
2023-08-26 23:21:17 -0400

    webpack configured and serice work path added for manifest injection

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -22,5 +22,45 @@ module.exports = () => {
         template: './src/index.html',
         chunks: ['main'],
       }),
-   
-    ]}}
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        filename: 'install.html',
+        chunks: ['install'],
+      }),
+      new WebpackPwaManifest({
+        name: 'JATE - Just Anotehr Text Editor',
+        short_name: 'JATE',
+        description: 'A simple text editor app.',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: path.resolve('src/assets/icons/icon_96x96.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          }
+        ]
+      }),
+      new InjectManifest({
+        swSrc: './client/src-sw.js', 
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
